84106c828d4fb28ce87f8454df2986cfb4c8dea2
klimkinadaria <klimkina_daria@mail.ru>
2025-09-20 19:35:13 +0200

    C1

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
 a = int(input("Введите число: "))
 b = int(input("Введите число: "))
-print(f"Разность: {a+b}")
-pr = a*b
-print("Произведение: {pr}")
-average = (a+b)/2
-print("Average: {average}")
